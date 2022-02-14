72b6a5ac2df17e41f174bdbd85518ef449e242a2
stocks0927 <1137720339@qq.com>
2022-02-14 16:00:50 +0800

    1244848

--- 占据字节大小.c
+++ 占据字节大小.c
@@ -8,5 +8,6 @@ int main()
 	printf("The size of long long is % d bytes\n", sizeof(long long));
 	printf("The size of float is % d bytes\n", sizeof(float));
 	printf("The size of double is % d bytes\n", sizeof(double));
+	printf("The size of double is % d bytes\n", sizeofdouble;//ю╗╨е©ирти╬ЁЩ
 	return 0;
 }
